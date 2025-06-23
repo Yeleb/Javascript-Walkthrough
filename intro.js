4df122ed89ccfcd385bc758f3c8e93d806324535
Caleb <calebzeringue25@gmail.com>
2025-06-23 01:10:24 -0500

    JS_1 Intro

---
+++ intro.js
@@ -0,0 +1,12 @@
+/*console.log("bruh");
+console.log("i like pizza");
+
+window.alert("This is an alert!");
+window.alert("I like pizza!");
+
+//This is a comment
+
+*/
+
+document.getElementById("myH1").textContent = "Hello";
+document.getElementById("myP").textContent = "I Like Pizza!";
