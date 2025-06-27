78b83a5e56f193d221a8eefedd398068393ab642
Yeleb <calebzeringue25@gmail.com>
2025-06-27 18:10:03 -0500

    JS_6 constants-circumference calc

---
+++ constants.js
@@ -0,0 +1,18 @@
+// constant = a variable that can't be changed
+//When const numbers and boolean name can be Uppercase as good practice
+//When const strings dont be uppercase
+
+
+const PI = 3.14159;
+let radius;
+let circumference;
+
+
+
+document.getElementById("mySubmit").onclick = function(){
+    radius = document.getElementById("myText").value;
+    radius = Number(radius);
+    circumference = 2 * PI * radius;
+    document.getElementById("myH3").textContent = `Circumference: ${circumference}cm`;
+
+}
