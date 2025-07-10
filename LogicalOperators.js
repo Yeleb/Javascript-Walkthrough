1c02749f5c274083c55b671db7553c63e118088a
Yeleb <calebzeringue25@gmail.com>
2025-07-10 18:47:35 -0500

    JS_17 Logical Operator

---
+++ LogicalOperators.js
@@ -0,0 +1,17 @@
+const temp = 20;
+
+if(temp > 0 && temp <= 30 ){
+    console.log("The weather is GOOD!")
+}
+else{
+    console.log("The weather is NOT GOOD!!")
+}
+
+const isSunny = true;
+
+if(!isSunny){
+    console.log("It is CLOUDY!")
+}
+else{
+    console.log("It is SUNNY!")
+}
